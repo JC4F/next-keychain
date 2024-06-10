a81777bc9f30b50d9e4fd91756f9135e06c2bfe2
JC4F <lequan12122002@gmail.com>
2024-06-10 07:49:26 +0700

    fix  build error

--- src/constants/fake-data/product.ts
+++ src/constants/fake-data/product.ts
@@ -5,6 +5,8 @@ export const productFakes: ProductTable[] = [
   {
     id: "1",
     title: "Moc 1",
+    externalProductId: "prod_QGKz1nyhdeeJc3",
+    externalPriceId: "price_1PPoJ6FoKPoHr2nxmuMVfLil",
     description: "Description 1",
     mainImage:
       "https://utfs.io/f/2ae10e8b-0492-49cf-a916-c9db01459eb4-1zya8.webp",
@@ -18,6 +20,8 @@ export const productFakes: ProductTable[] = [
     id: "2",
     title: "Moc 2",
     description: "Description 2",
+    externalProductId: "prod_QGKz1nyhdeeJc3",
+    externalPriceId: "price_1PPoJ6FoKPoHr2nxmuMVfLil",
     mainImage:
       "https://utfs.io/f/f96c65b1-39fa-49a3-8786-879acf12c41b-1zya9.webp",
     images: [],
